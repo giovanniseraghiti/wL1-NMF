0fab16770ca22aa2a1005cfe05cce47fe04a7cb2
Giovanni Seraghiti <giovanni.seraghiti@umons.ac.be>
2026-03-27 16:19:12 +0100

    Add files via upload

---
+++ tests/Figure_7_1_2.jl
@@ -0,0 +1,239 @@
+"""
+    In this Script we compare different NMF models on the MNIST data sets.
+    In particular, we test:
+        -L1NMF:  computed by the sCD algorithm
+        -FroNMF: computed by the HALS algorithm 
+        -L21NMF: computed by the MU algorithm
+        -KLNMF:  computed by the MU algorithm
+    We consider a noisy version of the MNIST data set where each white pixel is turned into black
+    and each grey is turned into white with probability p. Different percentages of noise are 
+    analyzed and the results are evaluated in terms of relative error with the ground truth in 
+    Frobenius norm ans Peak Signal to Noise Ratio (PSNR) 
+"""
+#Activate the test enviroment
+using Pkg
+Pkg.activate(@__DIR__)
+
+using MAT
+using SparseArrays
+using LinearAlgebra
+using Plots
+using Statistics
+using Random
+using L1NMF      
+
+# 7679 for image plot in the paper plus rng(4110) in matlab 
+Random.seed!(7679)
+
+#Open the data set
+data  = matopen("Dataset/MNIST_all.mat")
+#X_ts = Matrix{Float64}(sparse(read(data,"X"))); 
+X_ts = sparse(read(data,"X")); 
+dim=28  #images are 28x28
+
+#Select a portion of the Dataset
+samp=300   #number of samples (samp=300 for the results in the paper)
+ind_samp=randperm(60000)[1:samp];   #select random 
+X_t=X_ts[:,ind_samp]
+m, n      = size(X_t)
+X_t=X_t./255.0   #normalize
+normX=sqrt(sum(X_t.^2))
+
+#Algorithms options
+timelim=30.0  #for results in the paper use 90.0
+maxiter=200000
+tol=1e-6
+
+#Choose the rank of the factorization
+r=50 
+
+#Choose the penalization parameter of the wL1-NMF model
+lambda=1.0
+
+#Choose the smoothing parameter in the BCD algorithm with Nesterov smoothing 
+sigma=0.5 
+
+#Set the number of runs for each level of noise
+rep=1; 
+
+#Define the percentages of noise to add
+#noise_vec=[0.0,0.04,0.08,0.12,0.16,0.20] #choice in the paper
+noise_vec=[0.04,0.08]
+n_v=length(noise_vec)
+
+#Instantiate global variables
+true_err_l1=[]; true_err_l2=[]; true_err_l21=[];  true_err_KL=[]
+true_err_l1_vec=[]; true_err_l2_vec=[]; true_err_l21_vec=[]; 
+true_err_KL_vec=[]; true_err_n_vec=[];
+
+psnr_l1_vec=[]; psnr_l2_vec=[]; psnr_l21_vec=[]; 
+psnr_KL_vec=[];  psnr_n_vec=[]
+
+sparsity=[];
+
+D_l1=Dict(); D_l2=Dict(); D_l21=Dict(); D_kl=Dict();
+#Outer loop on the percentage of noise
+for i in 1:n_v
+    #add sparse noise noise: each white pixel has probability noise to 
+    #become black and each grey pixel has probability noise to turn white 
+    X=copy(X_t)
+    noise=noise_vec[i]
+    pos=findall(>(0.0), X_t)
+    np=Int64(length(pos)); 
+    ind_p=randperm(np)[1:Int64(trunc(noise*np))]
+    zer=findall(==(0.0), X_t)
+    nz=Int64(length(zer));
+    ind_z=randperm(nz)[1:Int64(trunc(noise*nz))]
+    X[pos[ind_p]].=0.0; X[zer[ind_z]].=1.0;   #X contains the noisy images
+    
+    #Sparsity of the matrix
+    nonz=nnz(sparse(X))
+    push!(sparsity,(m*n-nonz)/(m*n)*100)
+    
+    #compute peak signal 
+    mse_n = mean((X_t .- X).^2); 
+    push!(psnr_n_vec,10 * log10(1.0 / mse_n))
+    
+    #compute the error with groundtruth
+    push!(true_err_n_vec,sqrt(sum((X_t-X).^2))/normX)
+    
+    #Prepare the data set for the display of the images
+    n_disp=2;
+    X_disp=zeros(m,n_disp)
+    for k in 1:n_disp
+        X_disp[:,k]=reshape(reverse(reshape(X[:,k],dim,dim)'),m)
+    end
+    
+    psnr_l1=0; psnr_l2=0; psnr_l21=0; psnr_KL=0;
+    true_err_l1=0; true_err_l2=0; true_err_l21=0; true_err_KL=0;
+    T_l1=[]; T_l2=[]; T_l21=[]; T_KL=[];
+    #Inner loop: several runs for each method to average
+    for j in 1:rep
+        Random.seed!(j)
+        #Random initialization 
+        W0=rand(m,r); 
+        H0=rand(r,n)
+
+        #Rescale the initial point to match the norm of X
+        XHt = X*H0'; 
+        HHt = H0*H0'; 
+        scaling = sum(XHt.*W0)/sum( HHt.*(W0'*W0)); 
+        W0 = W0*scaling;
+        #Scale W and H so that columns/rows have the same norm, that is,  ||W(:,k)|| = ||H(k,:)|| for all k. 
+        W0, H0 =L1NMF.rescale(W0,H0)
+
+        #Compute FroNMF 
+        W_l2, H_l2 = FroNMF(X, r, W0=W0, H0=H0, timelim=timelim, maxiter=maxiter, tol=tol)
+        T_l2=W_l2*H_l2;
+        #Relative error with the true data on average
+        true_err_l2+=sqrt(sum((X_t-T_l2).^2))/normX/rep
+        #Compute PSNR
+        mse_l2 = mean((X_t .- T_l2).^2); 
+        psnr_l2+=10 * log10(1.0 / mse_l2)/rep
+
+        #Compute L21-NMF
+        W_l21, H_l21 = l21NMF(X, r, W0=W0, H0=H0,timelim=timelim, maxiter=maxiter, tol=tol)
+        T_l21=W_l21*H_l21;
+        #Relative error with the true data
+        true_err_l21+=sqrt(sum((X_t-T_l21).^2))/normX/rep
+        #Compute PSNR
+        mse_l21 = mean((X_t .- T_l21).^2); 
+        psnr_l21+=10 * log10(1.0 / mse_l21)/rep
+
+        #Compute KL-NMF
+        W_KL, H_KL = KLNMF(X, r, W0=W0, H0=H0,timelim=timelim, maxiter=maxiter, tol=tol)
+        T_KL=W_KL*H_KL;
+        #Relative error with the true data
+        true_err_KL+=sqrt(sum((X_t-T_KL).^2))/normX/rep
+        #Compute PSNR
+        mse_KL = mean((X_t .- T_KL).^2); 
+        psnr_KL+=10 * log10(1.0 / mse_KL)/rep
+
+        #Run a few iterations of HALS for FroNMF to initialize L1-NMF algorithms
+        W, H, times1,errors1 = FroNMF(X, r, W0=W0, H0=H0, maxiter=10)
+
+        #Compute wL1-NMF 
+        W_l1, H_l1 = sCD(X, r, lambda = lambda, W0=W, H0=H, timelim=timelim, maxiter=maxiter,tol=tol)
+        
+        #Relative error with the true data
+        T_l1=W_l1*H_l1
+        true_err_l1+=sqrt(sum((X_t-T_l1).^2))/normX/rep
+        #Compute PSNR
+        mse_l1 = mean((X_t .- T_l1).^2); 
+        psnr_l1+=10 * log10(1.0 / mse_l1)/rep
+
+        #Save the matrices for displaying
+        #Uncomment if you need to store the result (adjust the path)
+        if j==1
+            D_l1[i]=T_l1; D_l2[i]=T_l2; D_l21[i]=T_l21; D_kl[i]=T_KL
+            #Uncomment if you need to store the result
+            #matwrite("./results/MNIST_paper/app_l1$noise.mat",Dict("mat" => T_l1))
+            #matwrite("./results/MNIST_paper/app_l2$noise.mat",Dict("mat" => T_l2))
+            #matwrite("./results/MNIST_paper/app_l21$noise.mat",Dict("mat" => T_l21))
+            #matwrite("./results/MNIST_paper/app_KL$noise.mat",Dict("mat" => T_KL))
+        end 
+    end
+
+    #Save the averaged values for each noise level
+    push!(true_err_l1_vec,true_err_l1);  push!(true_err_l2_vec,true_err_l2); 
+    push!(true_err_l21_vec,true_err_l21); push!(true_err_KL_vec,true_err_KL);
+
+    push!(psnr_l1_vec,psnr_l1); push!(psnr_l2_vec,psnr_l2); 
+    push!(psnr_l21_vec,psnr_l21); push!(psnr_KL_vec,psnr_KL);
+ 
+end
+##
+
+#Plot the true error figure
+pl_t1=plot(noise_vec, true_err_l1_vec, label="L1-NMF", legend=:outertopright)
+pl_t1=plot!(noise_vec, true_err_l2_vec, label="L2-NMF")
+pl_t1=plot!(noise_vec, true_err_l21_vec, label="L21-NMF")
+pl_t1=plot!(noise_vec, true_err_KL_vec, label="KL-NMF")
+title!(pl_t1, "Comparison of NMF models")
+xlabel!(pl_t1, "Noise level (p)")
+ylabel!(pl_t1, "Relative Error with ground truth")
+display(pl_t1)
+
+#Plot the PSNR figure
+pl_t2=plot(noise_vec, psnr_l1_vec, label="L1-NMF", legend=:outertopright)
+pl_t2=plot!(noise_vec, psnr_l2_vec, label="L2-NMF")
+pl_t2=plot!(noise_vec, psnr_l21_vec, label="L21-NMF")
+pl_t2=plot!(noise_vec, psnr_KL_vec, label="KL-NMF")
+title!(pl_t2, "Comparison of NMF models")
+xlabel!(pl_t2, "Noise level (p)")
+ylabel!(pl_t2, "PSNR")
+display(pl_t2)
+
+##
+#Display and save the images
+ss=9;
+ind=randperm(samp)[1:ss]
+for i in 1:n_v
+    for k in ind
+        D_l1[i][:,k]=vec(reverse(reshape(D_l1[i][:,k],28,28)', dims=1))
+        D_l2[i][:,k]=vec(reverse(reshape(D_l2[i][:,k],28,28)', dims=1))
+        D_l21[i][:,k]=vec(reverse(reshape(D_l21[i][:,k],28,28)', dims=1))
+        D_kl[i][:,k]=vec(reverse(reshape(D_kl[i][:,k],28,28)', dims=1))
+    end
+end
+for i in 1:n_v 
+    L1NMF.affichage(D_l1[i][:,ind]', 28, 28, "L1_noise$(noise_vec[i]).png",nbimgperrow=3)
+    L1NMF.affichage(D_l2[i][:,ind]', 28, 28, "L2_noise$(noise_vec[i]).png",nbimgperrow=3)
+    L1NMF.affichage(D_l21[i][:,ind]', 28, 28, "L21_noise$(noise_vec[i]).png",nbimgperrow=3)
+    L1NMF.affichage(D_kl[i][:,ind]', 28, 28, "KL_noise$(noise_vec[i]).png",nbimgperrow=3)
+end
+##
+#Uncomment if you need to store the result (adjust the path)
+#Prepare the variables for storing
+#psnr_mat1=hcat(noise_vec,psnr_l1_vec,psnr_l2_vec,psnr_l21_vec,psnr_KL_vec,psnr_n_vec)
+#psnr_mat = Dict("matp" => psnr_mat1)
+#true_err_mat1=hcat(noise_vec,true_err_l1_vec,true_err_l2_vec,true_err_l21_vec,true_err_KL_vec,true_err_n_vec)
+#true_err_mat = Dict("mate" => true_err_mat1)
+#matwrite("./results/MNIST_paper/data_original.mat",Dict("Xt" => X_t))
+#matwrite("./results/MNIST_paper/noise_data$noise.mat",Dict("mat" => X))
+#matwrite("./results/MNIST_paper/sparsity.mat",Dict("spar" => sparsity))
+#matwrite("./results/MNIST_paper/gain.mat",Dict("g" => gain))
+#matwrite("./results/MNIST_paper/psnr_mat1.mat",psnr_mat)
+#matwrite("./results/MNIST_paper/true_err_mat1.mat",true_err_mat)
+
+
---
+++ tests/Figure_7_3.jl
@@ -0,0 +1,157 @@
+"""
+    In this Script we compare different choices of the regularization paramter in 
+    the wL1-NMF model. We use the sCD algorithm to compute the factorization.
+
+    We consider a matrix completion problem with false zeros on synthetic data.
+    We generate a random low rank matrix X=WH, we add Laplacian noise by 
+    X*=X+N and we replace a proportion of q_1 in [0,1) entries of X to zero,
+    siulating missing values. There are two types of zeros: a proportion of q_2 of type I, 
+    and of (1-q_2) for type II for q_2 in [0,1]: 
+        -type I sets the smallest entries of X to zero, for a total of q_1 q_2 mn entries. 
+         These entries can be seen as small entries thresholded to zero. We call them false zeros. 
+        -type II sets randomly selected entries of X to zero, for a total of q_1 (1-q_2) mn entries. 
+         We call them missing entries, as they do not bring any useful information since they were 
+         picked at random.  
+    These entries can be considered as missing. 
+    
+    We evaluate the results in terms of relative error with the ground truth low-rank matrix
+"""
+#Activate the test enviroment
+using Pkg
+Pkg.activate(@__DIR__)
+
+using SparseArrays
+using LinearAlgebra
+using Plots
+using DataFrames
+using CSV
+using Distributions
+using Random
+using L1NMF       #import the module, everything is used in the module is called like L1NMF.l2nmf
+
+#Set parameters for stopping criteria
+timelim=5.0
+maxiter=10
+tol=1e-6
+
+#Choose dimensions of the problem (paper setting: m=100, n=50, r=20)
+m=100; n=50; r=20
+
+#Number of runs (paper setting: rep=10)
+rep=1
+
+#Regularization parameter of the wL1-NMF model
+lambda_vec=[0.0,0.01,0.03,0.05,0.07,0.1,1.0]
+n_lam=length(lambda_vec)
+
+#Percentage of missing values over the total entries (both random and smallest missing)
+q=0.6 
+
+#Ratio between random missing entries and smallest missing entries
+ratio=[0.0,0.25,0.50,0.75,1.0]
+n_r=length(ratio)
+
+#Initialize the variable that stores the relative ground truth error
+res=zeros(n_r,n_lam)
+
+for k in 1:rep
+    #Fix the seed
+    Random.seed!(k)
+
+    #Generate the groundtruth matrix
+    W=rand(m,r); H=rand(r,n); X_t=W*H
+
+    #Set the parameter of the Laplacian noise
+    mu = 0.0   # location
+    b = 0.1   # scale
+    lap = Laplace(mu, b) #define Laplace distribution
+    # Generate a nosise matrix of size m x n
+    N = rand(lap, m, n)
+    #Noisy observation
+    X_n=X_t+N
+    #Check that the matrix we obtained is still nonnegative 
+    nonneg_check=sum(X_n.<0)
+    
+    #Cycle on the possible ratio between random missing values and smallest missing values
+    for t in 1:n_r
+        Random.seed!(1000+k)
+        X=copy(X_n)
+        #Create the matrix with missing values
+        p1=ratio[t] #Fraction of the missing values completely random
+        p2=1.0-p1 #Fraction of the missing the smallest values
+        num_zeros = round(Int, p1*q * m*n) #explicit number of indices to remove for random missing values
+        num_mis = round(Int, p2*q * m*n)   #explicit number of indices to remove for smallest missing values
+        
+        #Remove randomly missing data
+        entries = vcat(ones(Int, m*n - num_zeros), zeros(Int, num_zeros))
+        # Shuffle the entries randomly
+        shuffled = shuffle(entries)
+        # Reshape into m x n matrix
+        M=reshape(shuffled, m, n)   #mask of the random data to 
+        X=M.*X
+
+        #Remove the smallest num_mis entries among the remaining
+        xv=vec(X)
+        pos=findall(>(0.0), xv)  #remaining indices
+        ms=length(xv[pos])
+        idx = sortperm(vec(xv[pos]))[1:num_mis]  #take the indices associated to the smallest values
+        xv[pos[idx]].=0.0  
+        X=reshape(xv,m,n)  #Matrix containing both types of missing values
+
+        #Random initialization 
+        W0=rand(m,r);
+        H0=rand(r,n)
+
+        #Rescale the starting point for FroNMF
+        XHt = X*H0'; 
+        HHt = H0*H0'; 
+        scaling = sum(XHt.*W0)/sum( HHt.*(W0'*W0)); 
+        W0 = W0*scaling;
+        #Scale W and H so that columns/rows have the same norm, that is,  ||W(:,k)|| = ||H(k,:)|| for all k. 
+        W0, H0 =L1NMF.rescale(W0,H0)
+
+        # FroNMF for initialization (10 iter for the setting in the paper)
+        W, H, times1,errors1 = FroNMF(X, r, W0=W0, H0=H0, maxiter=10)
+
+        #wL1-NMF with sBCD algorithm
+        global rep0=copy(rep)
+        #Cicle on the regularization parameter of the wL1-NMF model
+        for j in 1: n_lam
+            #Set regularization parameter
+            lambda=lambda_vec[j] 
+            #Call the sCD algorithm
+            W_l1, H_l1, times_l1,errors_l1 = sCD(X, r, lambda = lambda, W0=W, H0=H, timelim=timelim, maxiter=maxiter, tol=tol) 
+            #Compute and store the error with the ground truth
+            global res[t,j]+=sqrt(sum((X_t-W_l1*H_l1).^2))/sqrt(sum((X_t).^2)) 
+        end
+        
+    end
+end
+#Average the results
+res./=rep
+
+
+#Plot the result: lambda vs rel. err. ground truth, each line is
+#a different ratio between random missing values and sallest missing values
+res[res .> 1] .= 1 #Avoid too large values for displaying purposes
+pl_t=nothing
+for t in 1:n_r
+    rr=ratio[t]
+    if t==1
+        global pl_t=plot(res[t,:], label="q_2=$rr", title="Sparsity q_1=$(q*100)%", xlabel="lambda", ylabel="rel. err. groud truth",
+        xticks=(1:n_lam, lambda_vec))
+    else
+        global pl_t=plot!(res[t,:], label="q_2=$rr",xticks=(1:n_lam, lambda_vec))
+    end
+end
+display(pl_t)
+
+
+#Save the results in a .csv file (uncomment if needed)
+#df = DataFrame(res, :auto) 
+#for j in 1:n_lam
+#    lam=lambda_vec[j]
+#    names(df)[j]="lambda=$lam"
+#end
+#Save on a CSV file
+#CSV.write("Rec_system_$q.csv", df)
